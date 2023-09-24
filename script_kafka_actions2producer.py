1317dc3b1e2c391d1ffe0c74bf32455170969ca1
Dmitry Orlov <37252057+DmitriiO@users.noreply.github.com>
2023-09-24 22:29:03 +0300

    update

--- script_kafka_actions2producer.py
+++ script_kafka_actions2producer.py
@@ -10,7 +10,8 @@ project_path='/usr/games/ActionsGenerator-CRM/'
 
 os.chdir(project_path)
 
-topic_name = 'data_from_crm_bot_mrf1'
+# Тема Kafka для подписки (по умолчанию) название бота в каждом МРФ
+topic_name = 'crm_bot_actions_mrf1'
 
 target_db = 'actions.db'
 
@@ -24,50 +25,58 @@ def read_and_send_data_to_kafka():
     try:
         conn = sqlite3.connect(target_db)
         cursor = conn.cursor()
-    except:
-        print(f'Ошибка подключения к {target_db}')
-    #  Выход из скрипта при ошибке подключения к БД
-        sys.exit()
     # Получение последнего смещения из метаданных
-    cursor.execute('SELECT last_transfer_timestamp FROM last_transfer_metadata')
-    last_offset = cursor.fetchone()[0]
+        cursor.execute('SELECT last_transfer_timestamp FROM last_transfer_metadata')
+        last_offset = cursor.fetchone()[0]
     
     # Выбор новых транзакций со смещением больше last_offset
-    cursor.execute('SELECT * FROM user_message WHERE date > ?', (last_offset,))
-    new_transactions = cursor.fetchall()
+        cursor.execute('SELECT * FROM user_message WHERE date > ?', (last_offset,))
+        new_transactions = cursor.fetchall()
     
-    # Отправка новых транзакций в Kafka в виде словаря по каждой отдельно
-    for transaction in new_transactions:
+    except:
+        
+        print(f'Ошибка подключения/чтения {target_db}')
+    #  Выход из скрипта при ошибке подключения к БД
+        sys.exit()
+   
+    else:
+        # Отправка новых транзакций в Kafka в виде словаря по каждой отдельно
+        for transaction in new_transactions:
     
-        add_transaction = {
-        
-            'user_id' : transaction[1],
-            'date' : transaction[2],
-            'filial' : transaction[3],
-            'week' : transaction[4],
-            'text1' : transaction[5],
-            'text2' : transaction[6],
-            'text3' : transaction[7],
-            'text4' : transaction[8],
-            'text5' : transaction[9],
-            'text6' : transaction[10],
-            'text7' : transaction[11],
-            'text8' : transaction[12],
-            'text9' : transaction[13],
-            'text10' : transaction[14],
-            'text11' : transaction[15],
-            'text12' : transaction[16],
-            'text13' : transaction[17],
-            'text14' : transaction[18],
-            'text15' : transaction[19]
-        }
-        
-        producer.send(topic_name, value = add_transaction)
-        
-    # Обновление смещения в метаданных после отправки транзакций в kafka - timestamp последней в стриме транзакции
-    cursor.execute('UPDATE last_transfer_metadata SET last_transfer_timestamp = ?', (transaction[2],))
+            message = {
+                'bot' : topic_name,
+                'user_id' : transaction[1],
+                'date' : transaction[2],
+                'filial' : transaction[3],
+                'week' : transaction[4],
+                'text1' : transaction[5],
+                'text2' : transaction[6],
+                'text3' : transaction[7],
+                'text4' : transaction[8],
+                'text5' : transaction[9],
+                'text6' : transaction[10],
+                'text7' : transaction[11],
+                'text8' : transaction[12],
+                'text9' : transaction[13],
+                'text10' : transaction[14],
+                'text11' : transaction[15],
+                'text12' : transaction[16],
+                'text13' : transaction[17],
+                'text14' : transaction[18],
+                'text15' : transaction[19]
+            }
 
-    # Закрытие Kafka Producer и соединения с базой данных
-    producer.close()
-    conn.commit()
-    conn.close()
+            producer.send(topic_name, value = message)
+        
+        # Обновление смещения в метаданных после отправки транзакций в kafka - timestamp последней в стриме транзакции
+        cursor.execute('UPDATE last_transfer_metadata SET last_transfer_timestamp = ?', (transaction[2],))
+
+        # Закрытие Kafka Producer и соединения с базой данных
+        producer.close()
+        conn.commit()
+        conn.close()
+        # Выход из скрипта
+        sys.exit()
+    
+if __name__ == "__main__":
+    read_and_send_data_to_kafka()
